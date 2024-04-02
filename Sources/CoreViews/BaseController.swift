1f807d114c4b671d9470302ec928fdee9af22d08
Cem Kılıç <glaitaht@gmail.com>
2024-04-02 15:29:20 +0300

    Project updated with base controller

---
+++ Sources/CoreViews/BaseController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cem Kılıç on 2.04.2024.
+//
+
+import UIKit
+
+open class BaseController: UIViewController {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        print("VC: BaseController Oluşturuldu.")
+    }
+}
